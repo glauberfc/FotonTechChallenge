ea5e1a6685887572c8281c38c7db469232e2936f
Glauber <glauber@MacBook-Pro-GlauberFC.local>
2018-11-17 09:04:25 -0300

    Remove unused styled-components config

--- frontend/src/styled-components-config.ts
+++ frontend/src/styled-components-config.ts
@@ -1,17 +1,13 @@
 // @ts-ignore
 import * as styledComponents from 'styled-components/native'
-// @ts-ignore
-import { ThemedStyledComponentsModule } from 'styled-components/native'
+import { ThemedStyledComponentsModule } from 'styled-components'
 
-import ThemeInterface from './theme'
+import { ThemeInterface } from './theme'
 
 const {
   default: styled,
-  css,
-  createGlobalStyle,
-  keyframes,
   ThemeProvider,
 } = styledComponents as ThemedStyledComponentsModule<ThemeInterface>
 
-export { css, createGlobalStyle, keyframes, ThemeProvider }
+export { ThemeProvider }
 export default styled
